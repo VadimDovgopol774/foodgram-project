490739599fe0bd9172659a5caeb640296bf6d799
Vadim7744 <dovgopol.vad@mail.ru>
2024-09-25 13:07:21 +0300

    ----

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,4 +2,4 @@ python manage.py collectstatic --noinput
 cp -r /app/static/. /backend_static/
 cp -r /app/media/. /backend_media/
 python manage.py migrate
-gunicorn foodgram.wsgi:application --bind 0:9090
+gunicorn foodgram.wsgi:application --bind 0:8888
